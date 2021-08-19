86d1cee3b305441b7c365020c9d6a68817a9814c
anugya-svg <anugyajain2003@gmail.com>
2021-08-19 23:37:47 +0530

    data.js file added

---
+++ src/data.js
@@ -0,0 +1,18 @@
+const Data=[
+    {
+        id:0,
+        color:1,
+        img:["https://www.cobragolf.com/media/catalog/product/cache/1e960c39b06c114f96be2f294eb9cd3b/1/9/194704-01-min.jpg"],
+        price:"10,000",
+        rating:4.2
+    },
+    {
+        id:1,
+        color:3,
+        img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_1500,h_1500/global/372849/18/sv01/fnd/IND/fmt/png/X-Ray-Game-Unisex-Shoes","https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_1500,h_1500/global/372849/01/sv01/fnd/IND/fmt/png/X-Ray-Game-Unisex-Shoes","https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_1500,h_1500/global/372849/11/sv01/fnd/IND/fmt/png/X-Ray-Game-Unisex-Shoes"],
+        price:"10,000",
+        rating:4.5
+    }
+    
+]
+export default Data;
