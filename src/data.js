b96f1ecdc2b7fb16e90f1c0676eac899a5c0c45a
anugya-svg <anugyajain2003@gmail.com>
2021-08-20 00:17:26 +0530

    data -11

--- src/data.js
+++ src/data.js
@@ -1,18 +1,123 @@
 const Data=[
     {
         id:0,
-        color:1,
-        img:["https://www.cobragolf.com/media/catalog/product/cache/1e960c39b06c114f96be2f294eb9cd3b/1/9/194704-01-min.jpg"],
+        count:1,
+        img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/194422/01/sv01/fnd/IND/fmt/png/PUMA-x-FIRST-MILE-FUSE-Men's-Training-Shoes"],
         price:"10,000",
-        rating:4.2
+        details:"Introducing the stylish, low-slung FUSE, designed based on insights from our own PUMA athletes and featuring the very best in stability and shock absorption.",
+        rating:4.2,
+        name:"PUMA x FIRST MILE FUSE Men's Training Shoes"
     },
     {
         id:1,
-        color:3,
+        count:3,
         img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_1500,h_1500/global/372849/18/sv01/fnd/IND/fmt/png/X-Ray-Game-Unisex-Shoes","https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_1500,h_1500/global/372849/01/sv01/fnd/IND/fmt/png/X-Ray-Game-Unisex-Shoes","https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_1500,h_1500/global/372849/11/sv01/fnd/IND/fmt/png/X-Ray-Game-Unisex-Shoes"],
-        price:"10,000",
-        rating:4.5
-    }
+        price:"8,000",
+        details:"Introducing the stylish, low-slung FUSE, designed based on insights from our own PUMA athletes and featuring the very best in stability and shock absorption.",
+        rating:4.5,
+        name:"X-Ray-Game-Unisex-Shoes"
+    },
+    {
+        id:2,
+        count:2,
+        img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/368846/07/sv01/fnd/IND/fmt/png/Respin-SL-Unsiex-Shoes","https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/368846/01/sv01/fnd/IND/fmt/png/Respin-SL-Unsiex-Shoes"],
+        price:"5,000",
+        details:"Introducing the stylish, low-slung FUSE, designed based on insights from our own PUMA athletes and featuring the very best in stability and shock absorption.",
+        rating:3.2,
+        name:"Respin-SL-Unsiex-Shoes"
+    },
+    {
+        id:3,
+        count:2,
+        img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/374766/09/sv01/fnd/IND/fmt/png/Court-Pure-SoftFoam+-Unisex-Sneakers","https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/374766/05/sv01/fnd/IND/fmt/png/Court-Pure-SoftFoam+-Unisex-Sneakers"],
+        price:"15,000",
+        details:"Introducing the stylish, low-slung FUSE, designed based on insights from our own PUMA athletes and featuring the very best in stability and shock absorption.",
+        rating:4.1,
+        name:"Court Pure SoftFoam Sneakers"
+    },
+    {
+        id:4,
+        count:2,
+        img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/369573/15/sv01/fnd/IND/fmt/png/PUMA-Rebound-Lay-Up-SoftFoam+-Unisex-Sneakers","https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/369573/03/sv01/fnd/IND/fmt/png/PUMA-Rebound-Lay-Up-SoftFoam+-Unisex-Sneakers"],
+        price:"3,745",
+        details:"Introducing the stylish, low-slung FUSE, designed based on insights from our own PUMA athletes and featuring the very best in stability and shock absorption.",
+        rating:3.2,
+        name:"PUMA Rebound Lay Up"
+    },
+    {
+        id:5,
+        count:1,
+        img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/381599/01/sv01/fnd/IND/fmt/png/PUMA-x-PWRFRAME-OP-1-Cyber-Unisex-Sneakers"],
+        price:"11,300",
+        details:"Introducing the stylish, low-slung FUSE, designed based on insights from our own PUMA athletes and featuring the very best in stability and shock absorption.",
+        rating:4.1,
+        name:"Cyber Unisex Sneakers"
+    },
+    {
+        id:6,
+        count:2,
+        img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/368863/01/sv01/fnd/IND/fmt/png/X-Ray-Lite-Rainbow-Shoes","https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/368863/02/sv01/fnd/IND/fmt/png/X-Ray-Lite-Rainbow-Shoes"],
+        price:"5,745",
+        details:"Introducing the stylish, low-slung FUSE, designed based on insights from our own PUMA athletes and featuring the very best in stability and shock absorption.",
+        rating:4.2,
+        name:"X-Ray-Lite-Rainbow-Shoes"
+    },
+    {
+        id:7,
+        count:3,
+        img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/371683/06/sv01/fnd/IND/fmt/png/Ralph-Sampson-Demi-OG--Sneakers","https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/371683/07/sv01/fnd/IND/fmt/png/Ralph-Sampson-Demi-OG--Sneakers","https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/371683/08/sv01/fnd/IND/fmt/png/Ralph-Sampson-Demi-OG--Sneakers"],
+        price:"3,745",
+        details:"Introducing the stylish, low-slung FUSE, designed based on insights from our own PUMA athletes and featuring the very best in stability and shock absorption.",
+        rating:3.2,
+        name:"Ralph-Sampson-Demi-OG"
+    },
+    {
+        id:7,
+        count:1,
+        img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/194419/04/sv01/fnd/IND/fmt/png/FUSE-Men's-Training-Shoes"],
+        price:"6,745",
+        details:"Introducing the stylish, low-slung FUSE, designed based on insights from our own PUMA athletes and featuring the very best in stability and shock absorption.",
+        rating:4.4,
+        name:"FUSE Men's Training Shoes"   
+    },
+    {
+        id:8,
+        count:1,
+        img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/105511/06/sv01/fnd/IND/fmt/png/PUMA-19.1-Bowling-Men's-Cricket-Shoes"],
+        price:"10,325",
+        details:"Introducing the stylish, low-slung FUSE, designed based on insights from our own PUMA athletes and featuring the very best in stability and shock absorption.",
+        rating:4.7,
+        name:"PUMA 19.1 Bowling Men's Cricket Shoes"  
+    },
+    {
+        id:9,
+        count:1,
+        img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/104709/01/sv01/fnd/IND/fmt/png/evoKNIT-FTB-II-FG-Men's-Football-Boots"],
+        price:"13,745",
+        details:"Introducing the stylish, low-slung FUSE, designed based on insights from our own PUMA athletes and featuring the very best in stability and shock absorption.",
+        rating:4.8,
+        name:"evoKNIT FTB II FG Men's Football Boots"  
+    },
+    {
+        id:10,
+        count:2,
+        img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/106309/03/sv01/fnd/IND/fmt/png/TACTO-IT-Men's-Football-Boots","https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/106309/06/sv01/fnd/IND/fmt/png/TACTO-IT-Men's-Football-Boots"],
+        price:"12,745",
+        details:"Introducing the stylish, low-slung FUSE, designed based on insights from our own PUMA athletes and featuring the very best in stability and shock absorption.",
+        rating:4.2,
+        name:"TACTO IT Men's Football Boots"  
+    },
+    {
+        id:11,
+        count:2,
+        img:["https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/194812/02/sv01/fnd/IND/fmt/png/Disc-Rebirth-Basketball-Shoes","https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_600,h_600/global/194812/01/sv01/fnd/IND/fmt/png/Disc-Rebirth-Basketball-Shoes"],
+        price:"18,745",
+        details:"Introducing the stylish, low-slung FUSE, designed based on insights from our own PUMA athletes and featuring the very best in stability and shock absorption.",
+        rating:4.2,
+        name:"Disc Rebirth Basketball Shoes" 
+    },
+
+
     
 ]
 export default Data;
