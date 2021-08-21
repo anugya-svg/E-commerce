cd143e6891255005f877091b516bc04ebdf02d7e
sandeep-07 <sandeepisthename007@gmail.com>
2021-08-21 10:28:16 +0530

    commit

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -29,25 +29,6 @@ function CartPage() {
                                 ))
                                 
                         }
-
-                        <tr className={styles.border}>
-                            <th scope="row" className="p-4">
-                                    <img className={styles.img_cart} src="https://assets.myntassets.com/h_720,q_90,w_540/v1/assets/images/11166294/2020/3/24/c099ba46-0d43-4684-a909-e8dc5709bb321585041823906-Levis-Men-Shirts-2061585041821702-1.jpg" alt="" />                                                                    
-                            </th>
-                            <td className="p-4">
-                                <h6>Levi's</h6>
-                                <p>Men Blue Slim fit solid shirt</p>
-                            </td>
-                            <td className="p-4">$239</td>
-                            <td className="p-4">
-                                <div className="d-flex">
-                                <span className={styles.qty}>+</span>
-                                <span className={styles.qty} style={{backgroundColor:"#e8e8e8"}}>1</span>
-                                <span className={styles.qty}>-</span>
-                                </div>
-                            </td>
-                            <td className="p-4">$360</td>
-                        </tr>
                     </tbody>
                 </table>
                 </div>
